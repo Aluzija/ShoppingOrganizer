f58ff6976f27aff46cc0198b2a258c1bc8b1b425
Paulina Sorys <poland_paulina@yahoo.pl>
2018-07-26 21:31:54 +0200

    Add test nested purchase form

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,3 +1,12 @@
 class DashboardsController < ApplicationController
-  def show; end
+  def show
+    @purchase = Purchase.new
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:unit_price, :amount, :expiration_date, :product_id,
+    product_attributes: [:id, :barcode, :name, :unit, :category])
+  end
 end
